3278644e9a52ed629c4ddd5e3b2a86dde2d3b1cb
Mohit Bhatnagar <78150489+mohit231007@users.noreply.github.com>
2026-08-09 21:44:12 +0530

    Load workspace handoff styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
+import "./handoff.css";
 import "./source-relationships.css";
 import "./styles.css";
 
